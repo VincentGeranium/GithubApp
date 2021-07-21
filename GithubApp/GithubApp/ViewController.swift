b7c4bca1b139eef581a55a24d8adc1dbdd343efe
VincentGeranium <kwangjun3952@gmail.com>
2021-07-21 17:49:43 +0900

    initial setup

---
+++ GithubApp/GithubApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubApp
+//
+//  Created by 김광준 on 2021/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
