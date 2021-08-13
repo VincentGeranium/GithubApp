37ae585b9841976076127a01bb902ac20e64b4f7
VincentGeranium <kwangjun3952@gmail.com>
2021-08-13 14:12:31 +0900

    Create generic custom body label.

---
+++ GithubApp/GithubApp/GithubFollowerBodyLabel.swift
@@ -0,0 +1,65 @@
+//
+//  GithubFollowerBodyLabel.swift
+//  GithubApp
+//
+//  Created by 김광준 on 2021/08/13.
+//
+
+import UIKit
+
+class GithubFollowerBodyLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        /*
+         c.f: About difference custom init parameter between 'GithubFollowerTitleLabel' and 'GithubFollowerBodyLabel'
+         The Body Label is gonna be dynaic type So, not gonna pass font size.
+         But the Body Label still passing textAlignment because some view's body can be left alignment other view's can be another alignment.
+         
+         c.f: Purpose of passing 'textAlignment'
+         Passing textAlignment for the configure text label.
+         */
+        
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = UIColor.secondaryLabel
+        
+        /*
+         Discussion: Why did I create 'preferredFont'?
+         Because I want this body label is gonna be dynamic.
+         When I assign the 'preferredFont(forTextStyle: .body)' to the body label font, It's going to be the size and will see Apple semantic UI.
+         */
+        font = UIFont.preferredFont(forTextStyle: .body)
+        
+        adjustsFontSizeToFitWidth = true
+        
+        /*
+         Discussion: For giving more ratio with body label text.
+         
+         Because body label have lots of text.
+         The Body label have more text than title.
+         So, give more text minimum scale factor than the title label.
+         In other word give less point than title label text.
+         It's gonna be smaller font size than title label text.
+         */
+        minimumScaleFactor = 0.75
+        
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    
+}
